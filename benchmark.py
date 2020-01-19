ec91536d9b9359870010886d7e1de4e3c7b2ad1f
Only1Loatheb <zielinski.x360@gmail.com>
2020-01-19 21:06:22 +0100

    Add results for variable number of particles.

--- benchmark.py
+++ benchmark.py
@@ -5,27 +5,19 @@ import json
 from tqdm import tqdm
 
 graph_paths = [
-  "graph100"
+  "graph1000"
 ]
 nrs_of_iterations = [
-  1000
+  10000
 ]
 
-# nrs_of_threads = [
-#   1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-#   1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-#   1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-#   1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-#   1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
-# ]
+particles_in = [
+  200, 
+  960, 
+  1920
+]
+
 nrs_of_threads = [
-  1, 1, 1, 1, 1,
-  2, 2, 2, 2, 2, 
-  3, 3, 3, 3, 3,
-  4, 4, 4, 4, 4,
-  5, 5, 5, 5, 5, 
-  6, 6, 6, 6, 6,
-  7, 7, 7, 7, 7, 
   8, 8, 8, 8, 8
 ]
 
@@ -39,21 +31,23 @@ data = []
 for graph_path in  tqdm(graph_paths, desc='graphs_progress'): 
   for nr_of_iteration in tqdm(nrs_of_iterations, desc='iterrs_progress'):
     for nr_of_threads in tqdm(nrs_of_threads, desc='threads_progress'):
-      bashCommand = f" mpirun -np {nr_of_threads} ./a.out 1 200 {nr_of_iteration} ./graphs/{graph_path}"
-      process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-      output, error = process.communicate()
-      # print(str(output),error)
-      duration, cost_best_path, _ =  output.decode("utf-8").split('\n')
-      dataPathName = f"{graph_path}_iters{nr_of_iteration}_threads{nr_of_threads}"
-      
-      single_data = {"name":             dataPathName,
-                    "graph":            graph_path,
-                    "nr_of_iteration":  nr_of_iteration,
-                    "nr_of_threads":    nr_of_threads,
-                    "duration":         duration,
-                    "cost_best_path":   cost_best_path
-                    }
-      data.append(single_data)
+      for particles in tqdm(particles_in, desc='particles_progress'):
+        bashCommand = f" mpirun -np {nr_of_threads} ./a.out 1 {particles} {nr_of_iteration} ./graphs/{graph_path}"
+        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+        output, error = process.communicate()
+        # print(str(output),error)
+        duration, cost_best_path, _ =  output.decode("utf-8").split('\n')
+        dataPathName = f"{graph_path}_iters{nr_of_iteration}_threads{nr_of_threads}_particles{particles}"
+        
+        single_data = {"name":             dataPathName,
+                      "graph":            graph_path,
+                      "nr_of_iteration":  nr_of_iteration,
+                      "nr_of_threads":    nr_of_threads,
+                      "duration":         duration,
+                      "cost_best_path":   cost_best_path,
+                      "particles":   particles
+                      }
+        data.append(single_data)
 with open(f"./results/{date_start}.json", "w") as json_file:
   json.dump(data, json_file, indent=4)
   json_file.write("\n")
